0e2da6562a40df35a7b3f1360c327fe703eb9f78
briansrt <brianstevenriofriotrochez@hotmail.com>
2025-11-03 13:59:37 -0500

    swagger

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(json())
 
 app.use(cors())
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/api/docs', swaggerUi.serve);
+app.get('/api/docs', swaggerUi.setup(swaggerFile));
 
 app.use('/user', userRoutes);
 app.use('/zapatos', zapatosRoutes);
